b2da381723b3dd685d9a284d2a6b8f4da7a30a70
Joe Chotard <jchotard@palantir.com>
2025-01-14 14:51:32 -0500

    to parent

--- src/h3spark/native.py
+++ src/h3spark/native.py
@@ -3,11 +3,33 @@ from pyspark.sql import functions as F
 
 H3_RES_OFFSET = 52
 H3_RES_MASK = 15 << H3_RES_OFFSET
+H3_RES_MASK_NEGATIVE = ~H3_RES_MASK
+H3_DIGIT_MASK = 7
+MAX_H3_RES = 15
+H3_PER_DIGIT_OFFSET = 3
+
 
 
 def get_resolution(col: Column) -> Column:
-    """Column must be of long type"""
     return F.shiftRight(col.bitwiseAND(H3_RES_MASK), H3_RES_OFFSET)
 
+def set_resolution_internal(col: Column, res: int) -> Column:
+    """Should probably not be used directly"""
+    return col.bitwiseAND(H3_RES_MASK_NEGATIVE).bitwiseOR(
+        res << H3_RES_OFFSET)
 
-# TODO: Add h3ToParent, h3ToChildren, h3MaxChild, h3MinChild, etc
+def set_index_digit(col: Column, res: int, digit: int) -> Column:
+    mask_shifted = H3_DIGIT_MASK << ((MAX_H3_RES - res) * H3_PER_DIGIT_OFFSET)
+    digit_shifted = digit << ((MAX_H3_RES - res) * H3_PER_DIGIT_OFFSET)
+    
+    return col.bitwiseAND(~mask_shifted).bitwiseOR(digit_shifted)
+
+
+def go_to_parent_fixed(col: Column, child_resolution: int, parent_resolution: int) -> Column:
+    """No validation, assume that all values of col are source_resolution + valid. Use at your own risk :)"""
+    assert child_resolution >= parent_resolution
+
+    parent = set_resolution_internal(col, parent_resolution)
+    for i in range(parent_resolution, child_resolution):
+        parent = set_index_digit(parent, i + 1, H3_DIGIT_MASK)
+    return parent
